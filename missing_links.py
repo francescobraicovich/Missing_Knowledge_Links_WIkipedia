b56bd857537d5958367b6a31fecebc14b36b30dd
francescobraicovich <francesco.braicovich@gmail.com>
2024-06-11 23:31:29 +0200

    feat: Refactor find_missing_links function to include specific node and cluster parameters

--- missing_links.py
+++ missing_links.py
@@ -1,7 +1,11 @@
 import networkx as nx
 import numpy as np
 
-def find_missing_links(graph, similarity_matrix, cluster_labels, threshold):
+def find_missing_links(graph, similarity_matrix, cluster_labels, threshold, specific_cluster=None, specific_node=None):
+
+    if specific_node is not None:
+        # find the cluster of the specific node
+        specific_cluster = cluster_labels[list(graph.nodes).index(specific_node)]
 
     adj_matrix = nx.to_numpy_array(graph)
     node_names = list(graph.nodes)
@@ -11,6 +15,11 @@ def find_missing_links(graph, similarity_matrix, cluster_labels, threshold):
 
     # find the number of clusters
     num_clusters = len(np.unique(cluster_labels))
+    min_range = 0
+
+    if specific_cluster is not None:
+        min_range = specific_cluster
+        num_clusters = specific_cluster + 1
 
     # iterate over the clusters
     for c in range(num_clusters):
@@ -19,23 +28,37 @@ def find_missing_links(graph, similarity_matrix, cluster_labels, threshold):
         cluster_indices = np.where(cluster_labels == c)[0]
 
         for i in range(len(cluster_indices)):
-            for j in range(i+1, len(cluster_indices)):
+            index_i = cluster_indices[i]
+            node_i = node_names[index_i]
 
-                index_i = cluster_indices[i]
-                index_j = cluster_indices[j]
+            if specific_node is not None and node_i != specific_node:
+                continue
 
-                # get the nodes
-                node_i = node_names[index_i]
-                node_j = node_names[index_j]
+            else:
+                for j in range(i+1, len(cluster_indices)):
 
-                # get the similarity score
-                similarity_score = similarity_matrix[index_i, index_j]
+                    
+                    index_j = cluster_indices[j]                
+                    node_j = node_names[index_j]
 
-                # check if the similarity score is below the threshold
-                if similarity_score >= threshold and adj_matrix[index_i, index_j] == 0:
-                    missing_links[(node_i, node_j)] = similarity_score
+                    # get the similarity score
+                    similarity_score = similarity_matrix[index_i, index_j]
+
+                    # check if the similarity score is below the threshold
+                    if similarity_score >= threshold and adj_matrix[index_i, index_j] == 0:
+                        missing_links[(node_i, node_j)] = similarity_score
 
     # order the missing links by similarity score
     missing_links = dict(sorted(missing_links.items(), key=lambda x: x[1], reverse=True))
 
     return missing_links
+
+def print_missing_links(missing_links, n=10):
+
+    print(f'Top {n} missing links:')
+    for i, (link, score) in enumerate(missing_links.items()):
+        print(f'{i}: {link[0]} <-- {score} --> {link[1]}')
+        if i == n:
+            break
+
+    print('')
