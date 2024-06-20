7c60a4138f2675a81d009fe7c0e1d23dfe365211
francescobraicovich <francesco.braicovich@gmail.com>
2024-06-20 13:25:55 +0200

    Refactor DBSCAN clustering algorithm for improved performance and flexibility

--- dbscan.py
+++ dbscan.py
@@ -3,8 +3,7 @@ from sklearn.cluster import HDBSCAN
 from sklearn.metrics import silhouette_score
 from scipy.optimize import minimize
 
-def dbscan_from_similarity(similarity_matrix, min_samples=2):
-    
+def dbscan_from_similarity(similarity_matrix):
     # find the maximum similarity score and normalise the similarity matrix
     max_similarity = np.max(similarity_matrix)
     similarity_matrix = similarity_matrix / max_similarity
@@ -14,11 +13,11 @@ def dbscan_from_similarity(similarity_matrix, min_samples=2):
 
     # find min and max cluster sizes
     n = np.shape(similarity_matrix)[0]
-    min_cluster_size = int(np.ceil(n/100))
-    max_cluster_size = int(np.ceil(n/10))
+    min_cluster_size = 10
+    max_cluster_size = 200
 
     # define a set of values for cluster_selection_epsilon
-    cluster_selection_epsilon_values = np.linspace(0.1, 0.5, 20)
+    cluster_selection_epsilon_values = np.linspace(0.01, 0.5, 15)
 
     # save the best silhouette score
     best_silhouette = 0
@@ -28,7 +27,7 @@ def dbscan_from_similarity(similarity_matrix, min_samples=2):
     for eps in cluster_selection_epsilon_values:
         
         # run HDBSCAN
-        dbscan = HDBSCAN(min_samples=min_samples, metric='precomputed', cluster_selection_epsilon=eps, 
+        dbscan = HDBSCAN(metric='precomputed', cluster_selection_epsilon=eps, min_samples=None, cluster_selection_method='eom',
                          min_cluster_size=min_cluster_size, max_cluster_size=max_cluster_size, n_jobs=-1)
         labels = dbscan.fit_predict(distance_matrix)
         
@@ -36,23 +35,23 @@ def dbscan_from_similarity(similarity_matrix, min_samples=2):
         num_clusters = len(np.unique(labels))
 
         # calculate the silhouette score
-        silhouette = silhouette_score(distance_matrix, labels)
+        silhouette = silhouette_score(1-similarity_matrix, labels)
 
-        print(f'Cluster Selection Epsilon: {eps}, Number of Clusters: {num_clusters}, Silhouette Score: {silhouette}')
+        print(f'Epsilon: {np.round(eps, 3)}, N Clusters: {np.round(num_clusters, 3)}, Silhouette Score: {np.round(silhouette, 3)}')
 
         # update the best silhouette score
-        if silhouette > best_silhouette:
+        if silhouette >= best_silhouette:
             best_silhouette = silhouette
             times_silhouette_not_improved = 0
             best_labels = labels
         else:
             times_silhouette_not_improved += 1
 
-        if times_silhouette_not_improved > 2:
+        if times_silhouette_not_improved > 3 and best_silhouette > 0:
             break
   
     # print the silhouette score
-    silhouette = silhouette_score(distance_matrix, best_labels)
+    silhouette = silhouette_score(1-similarity_matrix, best_labels)
 
     print(f'Silhouette Score: {silhouette}')
     
