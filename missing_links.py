0ca9ee28c5fbf0dc466d2193ecb8426e24f32e0f
francescobraicovich <francesco.braicovich@gmail.com>
2024-06-20 13:26:19 +0200

    Refactor find_missing_links function for improved performance and readability

--- missing_links.py
+++ missing_links.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy as np
 
-def find_missing_links(graph, similarity_matrix, cluster_labels, weak_threshold, 
+def find_missing_link_candidates(graph, similarity_matrix, cluster_labels, weak_threshold, 
                        strong_threshold, specific_cluster=None, specific_node=None):
 
     if specific_node is not None:
@@ -11,8 +11,11 @@ def find_missing_links(graph, similarity_matrix, cluster_labels, weak_threshold,
     adj_matrix = nx.to_numpy_array(graph)
     node_names = list(graph.nodes)
 
+    # initialise a missing link matrix
+    missing_link_matrix = np.zeros(np.shape(similarity_matrix))
+
     # initialise a missing link dictionary
-    missing_links = {}
+    missing_links_dict = {}
 
     # find the number of clusters
     num_clusters = len(np.unique(cluster_labels))
@@ -47,7 +50,8 @@ def find_missing_links(graph, similarity_matrix, cluster_labels, weak_threshold,
 
                     # check if the similarity score is below the threshold
                     if similarity_score >= weak_threshold and adj_matrix[index_i, index_j] == 0:
-                        missing_links[(node_i, node_j)] = similarity_score
+                        missing_links_dict[(node_i, node_j)] = similarity_score
+                        missing_link_matrix[index_i, index_j] = similarity_score
 
         # take the upper triangular part of the similarity matrix to avoid duplicates
         similarity_matrix = np.triu(similarity_matrix, k=1)
@@ -64,23 +68,24 @@ def find_missing_links(graph, similarity_matrix, cluster_labels, weak_threshold,
             node_i = node_names[i]
             node_j = node_names[j]
 
-            if (node_i, node_j) in missing_links or (node_j, node_i) in missing_links:
+            if (node_i, node_j) in missing_links_dict or (node_j, node_i) in missing_links_dict:
                 pass
             
             else:
-                missing_links[(node_i, node_j)] = similarity_matrix[i, j]
+                missing_links_dict[(node_i, node_j)] = similarity_matrix[i, j]
+                missing_link_matrix[i, j] = similarity_matrix[i, j]
 
     # order the missing links by similarity score
-    missing_links = dict(sorted(missing_links.items(), key=lambda x: x[1], reverse=True))
+    missing_links_dict = dict(sorted(missing_links_dict.items(), key=lambda x: x[1], reverse=True))
 
-    return missing_links
+    return missing_links_dict, missing_link_matrix
 
-def print_missing_links(missing_links, n=10):
+def print_missing_links_dict(missing_links_dict, n=10):
 
     print(f'Top {n} missing links:')
-    for i, (link, score) in enumerate(missing_links.items()):
-        print(f'{i}: {link[0]} <-- {score} --> {link[1]}')
-        if i == n:
+    for i, (link, score) in enumerate(missing_links_dict.items()):
+        print(f'{i}: {link[0]} <-- {np.round(score, 3)} --> {link[1]}')
+        if i == n:  
             break
 
     print('')
